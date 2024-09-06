075df89bd33eae08eabf2a4e496ccbe79983dca3
Fernando Bryan Reza Campos <fernandorezacampos@gmail.com>
2024-09-05 23:55:46 -0600

    Improve map management

---
+++ assets/maps/tiles/terrain-plus.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="terrain-plus" tilewidth="32" tileheight="32" tilecount="120" columns="15">
+ <image source="../../images/static/tilesets/terrain-plus.png" width="480" height="256"/>
+</tileset>
